80fb560135d42baad1dcd106fc00ef4f19b4deec
Joseph <josephmcdonough71@gmail.com>
2020-12-01 17:11:55 +0100

    first changes

--- wp-content/themes/theme_JPO/footer.php
+++ wp-content/themes/theme_JPO/footer.php
@@ -12,9 +12,24 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-
-		</div><!-- .site-info -->
+        <div class="footerflex">
+            <div class="footerlogo">
+                <h2 class="iutbmlogo">IUT Belfort-Montbéliard</h2>
+                <h2 class="univlogo">Université de Franche-Comté</h2>
+            </div>
+            <div class="coordonee">
+                <h2>IUT de Belfort-Montbéliard</h2>
+                <p>19 av. du Maréchal Juin | BP 527 <br>
+                    90016 Belfort cedex <br>
+                    tél. +33(0)3 84 58 77 00</p>
+            </div>
+            <ul style="list-style-type: none;">
+                <li><a href="">&rarr; Mentions légales</a></li>
+                <li><a href="">&rarr; Conditions générales d’utilisation</a></li>
+                <li><a href="">&rarr; Newsletter</a></li>
+            </ul>
+        </div>
+        <p>Etablissement public d’ens. supérieur, composante de l’Université de Franche-Comté</p>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
