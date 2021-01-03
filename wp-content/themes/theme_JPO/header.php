61dae9f4dc8c9600baefb2258f22e3bb4951cfff
Joseph <josephmcdonough71@gmail.com>
2021-01-03 05:35:49 +0100

    img reseaux

--- wp-content/themes/theme_JPO/header.php
+++ wp-content/themes/theme_JPO/header.php
@@ -61,13 +61,13 @@
 		</nav><!-- #site-navigation -->
         <div class="reseauxfixed">
             <ul>
-                <li class="twi"><a href="https://twitter.com/montbel_iut"><img src="http://localhost/test-theme/wp-content/uploads/2020/12/twitter.png" alt="twitter"/></a>
+                <li class="twi"><a href="https://twitter.com/montbel_iut"><img src="http://jpo-virtuelles.joseph-mcdonough.com/wp-content/uploads/2020/12/twitter.png" alt="twitter"/></a>
                 </li>
-                <li class="insta"><a href="https://www.instagram.com/mont_bel_iut"><img src="http://localhost/test-theme/wp-content/uploads/2020/12/instagram.png"
+                <li class="insta"><a href="https://www.instagram.com/mont_bel_iut"><img src="http://jpo-virtuelles.joseph-mcdonough.com/wp-content/uploads/2020/12/instagram.png"
                                                                                         alt="Instagram"/></a></li>
                 <li class="ytb"><a href="https://www.youtube.com/channel/UCOipQ939Yy0Ja5GVI4l-lvA"><img
-                                src="http://localhost/test-theme/wp-content/uploads/2020/12/youtube.png" alt="Youtube"/></a></li>
-                <li class="fb"><a href="#"><img src="http://localhost/test-theme/wp-content/uploads/2020/12/facebook.png" alt="Facebook"/></a></li>
+                                src="http://jpo-virtuelles.joseph-mcdonough.com/wp-content/uploads/2020/12/youtube.png" alt="Youtube"/></a></li>
+                <li class="fb"><a href="#"><img src="http://jpo-virtuelles.joseph-mcdonough.com/wp-content/uploads/2020/12/facebook.png" alt="Facebook"/></a></li>
             </ul>
         </div>
 	</header><!-- #masthead -->
